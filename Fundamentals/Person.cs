3edaddfd9faae9a44fe213d8c7523be1b14917ae
baleygr-ofnir <jorma.lindh@mimirsbrunnr.cloud>
2025-08-28 14:31:26 +0200

    Combining everything

--- Fundamentals/Person.cs
+++ Fundamentals/Person.cs
@@ -2,14 +2,10 @@ using System.Collections;
 
 namespace Fundamentals;
 
-class Person(string firstName, string lastName, int age, string hobby) : IEnumerable
+class Person(string firstName, string lastName, int age, string hobby)
 {
     public string FirstName = firstName;
     public string LastName = lastName;
     public int Age = age;
     public string Hobby = hobby;
-    public IEnumerator GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
 }
--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -2,6 +2,7 @@
 
 internal class Program
 {
+    /*
     static string[] _messages =
     [
         "In this lesson we will start slowly.",
@@ -10,14 +11,64 @@ internal class Program
         "    3. Make decisions with if-else"
     ];
     static Person _person  = new Person(firstName: "Baleygr", lastName: "Fjoelsviethr", age: 20000, hobby: "the pursuit for domination of every realm");
+    */
     
     static void Main(string[] args)
     {
-        foreach (var message in _messages)
-        {
-            Console.WriteLine(message);
-        }
+        // foreach (var message in _messages)
+        // {
+        //     Console.WriteLine(message);
+        // }
+        //
+        // Console.WriteLine($"{_person.FirstName} {_person.LastName} is {_person.Age} years old and enjoys {_person.Hobby} as a pastime");
 
-        Console.WriteLine($"{_person.FirstName} {_person.LastName} is {_person.Age} years old and enjoys {_person.Hobby} as a pastime");
+        /*Console.WriteLine("Enter grade: ");
+        char? grade = Console.ReadKey().KeyChar;
+        
+        if (grade == 'A')
+        {
+            Console.WriteLine("\nGood job.");
+        }
+        else if (grade == 'B')
+        {
+            Console.WriteLine("\nDecent.");
+        }
+        else
+        {
+            Console.WriteLine("\nWork harder.");
+        }
+        
+        switch (grade)
+        {
+           case 'A':
+               Console.WriteLine("\nGood job.");
+               break;
+           case 'B':
+               Console.WriteLine("\nDecent.");
+               break;
+           case 'C':
+               Console.WriteLine("\nWork harder.");
+               break;
+        }*/
+        Console.WriteLine("What is your name?");
+        string? firstName = Console.ReadLine();
+        Console.WriteLine($"Hello {firstName}, nice to meet you!");
+        
+        Console.WriteLine("What is your age?");
+        int age;
+        bool isInt = int.TryParse(Console.ReadLine(), out age);
+        Console.WriteLine($"You are {age} years old.");
+        
+        Console.WriteLine("Do you like pizza? (y/n)? ");
+        string? likePizza = Console.ReadLine()?.ToUpper();
+
+        if (likePizza == "Y")
+        {
+            Console.WriteLine("Nice");
+        }
+        else
+        {
+            Console.WriteLine("Sad");
+        }
     }
 }
